1229ed4568b6194276dff8a6c0cc0f5780b5c050
Brian Noh <noh24@ymail.com>
2022-12-04 19:44:58 -0500

    add code to namespace

---
+++ Models/Car.cs
@@ -0,0 +1,20 @@
+namespace Dealership.Models
+{
+  public class Car
+  {
+    public string MakeModel;
+    public int Price;
+    public int Miles;
+    //constructor
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price <= maxPrice);
+    }
+  }
+}
