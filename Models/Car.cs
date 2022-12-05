1a517a54b829a330448636abaca84242a366d7da
Brian Noh <noh24@ymail.com>
2022-12-04 22:19:55 -0500

    remove getter and setter methods, add properties

--- Models/Car.cs
+++ Models/Car.cs
@@ -2,40 +2,40 @@ namespace Dealership.Models
 {
   public class Car
   {
-    private string _makeModel;
-    private int _price;
-    private int _miles;
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
     //constructor
     public Car(string makeModel, int price, int miles)
     {
-      _makeModel = makeModel;
-      _price = price;
-      _miles = miles;
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
     }
     public static string MakeSound(string sound)
     {
       return $"Our cars sound like {sound}";
     }
-    // getter method
-    public string GetMakeModel()
-    {
-      return _makeModel;
-    }
-    public int GetPrice()
-    {
-      return _price;
-    }
-    public int GetMiles()
-    {
-      return _miles;
-    }
-    public void SetPrice(int newPrice)
-    {
-      _price = newPrice;
-    }
+    // // getter method
+    // public string GetMakeModel()
+    // {
+    //   return _makeModel;
+    // }
+    // public int GetPrice()
+    // {
+    //   return _price;
+    // }
+    // public int GetMiles()
+    // {
+    //   return _miles;
+    // }
+    // public void SetPrice(int newPrice)
+    // {
+    //   _price = newPrice;
+    // }
     public bool WorthBuying(int maxPrice)
     {
-      return (_price <= maxPrice);
+      return (Price <= maxPrice);
     }
   }
 }
